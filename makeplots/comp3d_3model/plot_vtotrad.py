3b6c814a77e992a6d8e29fff9b4b3ab251b51a5e
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2023-05-08 17:17:17 -0500

    partial debug; some qt plugin issue on quest

--- makeplots/comp3d_3model/plot_vtotrad.py
+++ makeplots/comp3d_3model/plot_vtotrad.py
@@ -3,10 +3,12 @@ import matplotlib.colors as mcolors
 import matplotlib.gridspec as gsp
 import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
+from matplotlib.transforms import Transform
 import numpy as np
 
 import fire_an.utils.math_utils as mu
 import fire_an.makeplots.tol_colors as tc
+import fire_an.simlists as sl
 
 def getdata_hist(filen, yunit=1., xunit=1.):
     with h5py.File(filen, 'r') as f:
@@ -16,7 +18,7 @@ def getdata_hist(filen, yunit=1., xunit=1.):
             hist = 10**hist
         rbins = f['axis_0/bins'][:] / xunit
         ybins = f['axis_1/bins'][:] / yunit   
-        csmpath = 'Header/inputpars/cosmopars'
+        csmpath = 'Header/cosmopars'
         cosmopars = {key: val for key, val in f[csmpath].items()}
     basehist = hist / np.diff(rbins)[:, np.newaxis] \
                     / np.diff(ybins)[np.newaxis, :] \
@@ -32,7 +34,8 @@ def getdata_hist(filen, yunit=1., xunit=1.):
             'cosmopars': cosmopars}
 
 def plot_vradtot_zphyscomp(filen_template, zfills, phicfills,
-                           title=None, outname=None, ylabel=None):
+                           title=None, outname=None, ylabel=None,
+                           physlabels=None):
     '''
     different redshifts in each panel, different panels for different
     phys. vars.
@@ -89,6 +92,13 @@ def plot_vradtot_zphyscomp(filen_template, zfills, phicfills,
         ax.tick_params(labelsize=fontsize - 1., which='both',
                        direction='in', top=True, right=True,
                        labelbottom=True, labelleft=doleft)
+        if physlabels is not None:
+            plab = physlabels[pi]
+            ax.text(0.05, 0.95, plab, fontsize=fontsize,
+                    horizontalalignment='left',
+                    verticalalignment='top',
+                    transform=ax.transAxes,
+                    color='black')
         if doleft and ylabel is not None:
             ax.set_ylabel(ylabel, fontsize=fontsize)
         ax.set_xlabel(xlabel, fontsize=fontsize)
@@ -105,6 +115,13 @@ def plot_vradtot_zphyscomp(filen_template, zfills, phicfills,
                 percentiles=np.array(perclevels))
             for pvi in range(len(perclevels)):
                 ax.plot(rc, yv[pvi], color=color, **(pstyles[pvi]))
+    ylims = [ax.get_ylims() for ax in axes]
+    ymin = min([yl[0] for yl in ylims])
+    ymin = max(ymin, -250.)
+    ymax = max([yl[1] for yl in ylims])
+    ymax = min(ymax, 400.)
+    [ax.set_ylims((ymin, ymax)) for ax in axes]
+
     chandles = [mlines.line2D((), (), color=color, label=f'$z={z:.1f}$')
                 for color, z in zip(colors, zvals)]
     lshandles = [mlines.line2D((), (), color='black', 
@@ -115,6 +132,51 @@ def plot_vradtot_zphyscomp(filen_template, zfills, phicfills,
                loc='upper center', bbox_to_anchor=(0.5, 0.65))
     if outname is not None:
         plt.savefig(outname, bbox_inches='tight')
+
+def plotset_vradtot_zphyscomp(vtype='rad'):
+    fdir = '/projects/b1026/nastasha/hists/vradtot_all2/'
+    outdir = '/projects/b1026/nastasha/imgs/vel3dcomp/'
+    simnames = sl.m13_sr_all2 + sl.m13_hr_all2 \
+               + sl.m12_sr_all2 + sl.m12_hr_all2
+    sn_hr = sl.m13_hr_all2 + sl.m12_hr_all2
+    sn_sr = sl.m13_sr_all2 + sl.m12_sr_all2
+    icsets = []
+    ylabel = (f'$v_{vtype} \\;'
+              '[\\mathrm{km} \\, \\mathrm{s}^{-1}]$')
+    for simname in simnames:
+        ic = simname.split('_')[0]
+        setind = np.where([np.all([_sn.startswith(ic) for _sn in icset])
+                           for icset in icsets])[0]
+        if len(setind) == 0:
+            icsets.append([simname])
+        else:
+            icsets[setind[0]].append(simname)
+    weights = ['gasmass', 'gasvol', 'Neon', 'Ne8', 'Mg10', 'O6']
+    for icset in icsets:
+        for weight in weights:
+            ftemp = fdir + \
+                    (f'hist_v{vtype}_by_{weight}_{{simname}}'
+                    '_snap{snapnum}_bins1_v1_hvcen.hdf5')
+            phicfills = [{'simname': simname} for simname in icset]
+            zfills = [[{'snapnum': snap} for snap in sl.snaps_hr] 
+                      if simname in sn_hr else 
+                      [{'snapnum': snap} for snap in sl.snaps_sr]
+                      if simname in sn_sr else 
+                      None 
+                      for simname in icset]
+            physlabels = ['noAGN' if '_sdp1e10_' in simname
+                          else 'AGN-CR' if 'MHDCRspec1' in simname
+                          else 'AGN-noCR'
+                          for simname in icset]
+            ic = icset[0].split('_')[0]
+            title = f'{ic} velocities, weighted by {weight}'
+            outname = outdir + f'{ic}_zphyscomp_v{vtype}_by_{weight}_r3D.pdf'
+            
+            plot_vradtot_zphyscomp(ftemp, zfills, phicfills,
+                                   title=title, outname=outname, 
+                                   ylabel=ylabel, physlabels=physlabels)
+            break
+        break
 
 def plotvradtot_zweightcomp(filen_template, zfills, wfills):
     '''

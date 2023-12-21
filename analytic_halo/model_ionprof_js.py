6bed470c6e574fc1746e91ca556e53a3dcbde29c
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2023-12-21 15:40:22 -0600

    added run set 1, ion prof checks

--- analytic_halo/model_ionprof_js.py
+++ analytic_halo/model_ionprof_js.py
@@ -13,21 +13,34 @@ import scipy.interpolate as spi
 import fire_an.ionrad.ion_utils as iu
 import fire_an.utils.constants_and_units as c
 import fire_an.utils.cosmo_utils as cu
+import fire_an.utils.math_utils as mu
 import fire_an.utils.opts_locs as ol
+import fire_an.mstar_mhalo.loader_smdpl_sfr as lds
 
-sys.path.insert(0, ol.path_jscoolingflow)
+sys.path.insert(1, ol.path_jscoolingflow)
 import cooling_flow.cooling_flow as cf
 import cooling_flow.WiersmaCooling as wcool
 import cooling_flow.HaloPotential as halopot
 
-outdir_profiles = ('/Users/nastasha/ciera/projects_lead/fire3_ionabs/'
-                   'analytical/')
+#outdir_profiles = ('/Users/nastasha/ciera/projects_lead/fire3_ionabs/'
+#                    'analytical/')
+outdir_profiles = '/projects/b1026/nastasha/imgs/analytical/savedprof/'
 # Planck 2015 from astropy, used in Jonathan Stern's model
 cosmo_base = {'h': 0.6774, 'omegam': 0.3075, 'omegab': 0.0486}
 cosmo_base['omegalambda'] = 1. - cosmo_base['omegam']
 
-def solutionset(logmvirs_msun, redshift, mdot=1. * cf.un.Msun / cf.un.yr,
-                zsol=0.1, plind=-0.1):
+
+def get_sfrs_mh(logmhs_msun, z=0.75, percentiles=(0.16, 0.5, 0.84)):
+    histobj = lds.SFRHMhists(np.array([z]))
+    sfrs_tab, mhs_tab = histobj.getperc_sfrmh(z, mode='mhtosfr', 
+                                              percvals=np.array(percentiles))
+    out = [[mu.linterpsolve(mhs_tab, sfrs_tab_perc, mh_this) 
+            for sfrs_tab_perc in sfrs_tab]
+           for mh_this in logmhs_msun]
+    return np.array(out)
+
+def solutionset(logmvirs_msun, redshift, mdotperc=(0.16, 0.5, 0.84), 
+                zsol=0.3, plind=-0.1):
     '''
     get density/temperature solutions for a given halo mass set (BN98
     halo masses), mass flow rate (mdot), metallicity (zsol), 
@@ -44,35 +57,37 @@ def solutionset(logmvirs_msun, redshift, mdot=1. * cf.un.Msun / cf.un.yr,
     cosmopars['a'] = 1. / (1. + redshift)
     rvirs_cm = cu.rvir_from_mvir(mvirs_msun * c.solar_mass,
                                   cosmopars, meandef='BN98')
-    rvirs_kpc = rvirs_cm / (c.cm_per_mpc * 1e-3) * cf.un.kpc
+    rvirs_kpc = rvirs_cm / (c.cm_per_mpc * 1e-3)
     vcs_cmps = np.sqrt(c.gravity * mvirs_msun * c.solar_mass / rvirs_cm)
-    print(vcs_cmps / 1e5)
+    
+    mdots = 10**get_sfrs_mh(logmvirs_msun, z=redshift, percentiles=mdotperc)
+
     potentials = [halopot.PowerLaw(plind, vc_cmps * cf.un.cm / cf.un.s, 
-                                   rvir_kpc)
+                                   rvir_kpc * cf.un.kpc)
                   for vc_cmps, rvir_kpc in zip(vcs_cmps, rvirs_kpc)]
     cooling = wcool.Wiersma_Cooling(zsol, redshift)
     # want to shoot from Rcirc, since the halos considered here will
     # generally be fully virialized. (The simulated ones sure seem to
     # be.)
     solutions = {}
-    for lmv, potential in zip(logmvirs_msun, potentials):
+    for lmv, potential, mdotsub in zip(logmvirs_msun, potentials, mdots):
         print(lmv)
         rcirc = 0.02 * potential.Rvir # inner stalled radius
-        rmax = 2. * potential.Rvir
-        #print(rcirc, rmax)
-        #print(potential.vc(rcirc))
-        #print(potential.vc(rmax))
-        solution = cf.shoot_from_R_circ(potential, cooling, 
-                                        rcirc, mdot, rmax, 
-                                        v0=1. * cf.un.km/cf.un.s,
-                                        max_step=0.1, 
-                                        T_low=1e4 * cf.un.K, 
-                                        T_high=3e7 * cf.un.K,
-                                        tol=1e-6, epsilon=0.1,
-                                        terminalUnbound=True,
-                                        pr=True, 
-                                        return_all_results=False)
-        solutions[lmv] = solution
+        # allow +-2 Rvir los integration of ion density profiles
+        # to impact parameter 2 Rvir from center
+        rmax = 10. * potential.Rvir # np.sqrt(2) * 2.
+        solutions[lmv] = {}
+        for mdot in mdotsub:
+            #print(rcirc, rmax)
+            #print(potential.vc(rcirc))
+            #print(potential.vc(rmax))
+            solution = cf.shoot_from_R_circ(potential, cooling, 
+                rcirc, mdot * cf.un.Msun / cf.un.yr, rmax, 
+                v0=1. * cf.un.km / cf.un.s, max_step=0.1, 
+                T_low=1e4 * cf.un.K, T_high=3e7 * cf.un.K,
+                tol=1e-6, epsilon=0.1, terminalUnbound=True,
+                pr=True, return_all_results=False)
+            solutions[lmv][mdot] = solution
     return solutions
 
 def calc_cgmfrac(solution, mvir_msun):
@@ -122,25 +137,26 @@ def calcionprof(solution, ion, redshift, zsol,
     _iondens_prof = spi.interp1d(np.log10(r_cm), np.log10(iondens), 
                                  kind='linear',
                                  fill_value=0.)
-    id0 = _iondens_prof(np.log10(truncate_inner_cm))
+    lid0 = _iondens_prof(np.log10(truncate_inner_cm))
     _r_cm = np.append(truncate_inner_cm, r_cm[rimin:])
-    _iondens = np.append(id0, iondens[rimin:])
-    iondens_prof = spi.interp1d(np.log10(_r_cm), np.log10(_iondens), 
+    _liondens = np.append(lid0, np.log10(iondens)[rimin:])
+    liondens_prof = spi.interp1d(np.log10(_r_cm), _liondens, 
                                  kind='linear',
                                  fill_value=0.)
-
-    logr3d_cm = 0.5 * np.log10(impactpars_cm[:, np.newaxis]**2,
-                               lossample_cm[np.newaxis, :]**2)
-    dl = 0.5 * (lossample_cm[2:] - lossample_cm[:-2])
-    dl = np.append(dl[0], dl)
-    dl = np.append(dl, dl[-1])
-    coldens = np.sum(iondens_prof(logr3d_cm) * dl[np.newaxis, :], axis=1)
+    loscens_cm = 0.5 * (lossample_cm[1:] + lossample_cm[:-1])
+    logr3d_cm = 0.5 * np.log10((impactpars_cm[:, np.newaxis]/1e21)**2 +
+                               (loscens_cm[np.newaxis, :]/1e21)**2) \
+                + 21.
+    dl = lossample_cm[1:] - lossample_cm[:-1]
+    iondens = 10**liondens_prof(logr3d_cm)
+    coldens = np.sum(iondens * dl[np.newaxis, :], axis=1)
+    #print('coldens: ', coldens)
     return coldens
 
-def savesol(solution, ion, redshift, zsol, plind, mdot, logmvir_msun,
-            outfilen):
+def savesol(solution, ion, redshift, zsol, plind, mdot, pmdot, 
+            logmvir_msun, outfilen):
     grpn = (f'z{redshift:.2f}_Zsolar{zsol:.2e}_vcplind{plind:.2f}'
-            f'mdotMsunperYr{mdot:.2e}_logmvirMsun{logmvir_msun:.2f}')
+            f'_mdotperc{pmdot:.3f}_logmvirMsun{logmvir_msun:.2f}')
     if solution is None:
         with h5py.File(outfilen, 'a') as f:
             if grpn in f:
@@ -152,9 +168,9 @@ def savesol(solution, ion, redshift, zsol, plind, mdot, logmvir_msun,
 
     fcgm = calc_cgmfrac(solution, 10**logmvir_msun)
     rvir_cm = solution.potential.Rvir.to('cm').value
-    truncate_inner_cm = 0.1 * rvir_cm
-    impactpars_cm = np.arange(0., 1.52, 0.05) * rvir_cm
-    lossample_cm = np.arange(0., 1.502, 0.005) * rvir_cm
+    truncate_inner_cm = 0.09 * rvir_cm
+    impactpars_cm = np.arange(0.1, 2.02, 0.05) * rvir_cm
+    lossample_cm = np.arange(-2., 2.002, 0.005) * rvir_cm
     coldens_cm2 = calcionprof(solution, ion, redshift, zsol,
                               impactpars_cm, lossample_cm, 
                               truncate_inner_cm)
@@ -166,6 +182,7 @@ def savesol(solution, ion, redshift, zsol, plind, mdot, logmvir_msun,
         grp.attrs.create('Z_solar', zsol)
         grp.attrs.create('vcplind', plind)
         grp.attrs.create('mdot_MsunperYr', mdot)
+        grp.attrs.create('mdot_percentile_at_Mvir', pmdot)
         grp.attrs.create('failed', False)
         grp.attrs.create('logMvir_Msun_BN98', logmvir_msun)
         grp.create_dataset('R_kpc', data=solution.Rs().to('kpc').value)
@@ -179,27 +196,41 @@ def savesol(solution, ion, redshift, zsol, plind, mdot, logmvir_msun,
         sgrp.create_dataset('coldens_cm2', data=coldens_cm2)
     return None
 
-def runsolsgrid(outfilen='test1.hdf5'):
-    logmvirs_msun = np.arange(11.5, 13.65, 0.1)
+# test1: col. dens. values are negative and of unreasonably 
+#        large magnitude. R_kpc, T_K, nH_cm3 look ok.
+# test2: issue seems to have been with log/lin ion fracs.
+# note that lower Mvir values do not give a solution; this will
+# be before inner CGM virialization
+# test3: looks ok
+# set1: rmax = np.sqrt(2) * 2. * potential.Rvir 
+#       T_low=1e4 * cf.un.K, 
+#       T_high=1e8 * cf.un.K,
+#       tol=1e-6, epsilon=0.1, terminalUnbound=True,
+#       pr=True, return_all_results=False
+# set2: same as set1, but rmax = 10. * potential.Rvir 
+def runsolsgrid(outfilen='set1_jsmodel.hdf5'):
+    logmvirs_msun = np.arange(11.0, 13.65, 0.1)
     #redshifts = np.arange(0.5, 1.05, 0.1)
     redshifts = np.array([0.75])
     #zs_sol = np.array([0.1, 0.2, 0.5, 1.])
-    zs_sol = np.array([0.1, 1.])
+    zs_sol = np.array([0.1, 0.3, 1.])
     #plinds = np.array([0., -0.1, -0.2, -0.5])
-    plinds = np.array([-0.1])
+    plinds = np.array([0., -0.1, -0.2])
     #mdots = np.array([0.01, 0.03, 0.1, 0.3, 1., 3., 10., 30., 100.])
-    mdots = np.array([0.01, 0.1, 1., 10., 100.])
+    pmdots = np.array([0.16, 0.5, 0.84])
     
     for redshift in redshifts:
         for zsol in zs_sol:
             for plind in plinds:
-                for mdot in mdots:
-                    sols = solutionset(logmvirs_msun, redshift, 
-                                        mdot=mdot * cf.un.Msun / cf.un.yr,
-                                        zsol=zsol, plind=plind)
-                    for sol in sols:
-                        savesol(sols[sol], 'Ne8', redshift, zsol, 
-                                plind, mdot, sol,
+                sols = solutionset(logmvirs_msun, redshift, 
+                                    mdotperc=pmdots, 
+                                    zsol=zsol, plind=plind)
+                for mhsol in sols:
+                    sfsols = list(sols[mhsol].keys())
+                    sfsols.sort()
+                    for sfsol, pmdot in zip(sfsols, pmdots):
+                        savesol(sols[mhsol][sfsol], 'Ne8', redshift, zsol,
+                                plind, sfsol, pmdot, mhsol,
                                 outdir_profiles + outfilen)
 
 
--- analytic_halo/plot_ionprof_js.py
+++ analytic_halo/plot_ionprof_js.py
@@ -2,15 +2,17 @@ import h5py
 import matplotlib.cm as mcm
 import matplotlib.colors as mcolors
 import matplotlib.gridspec as gsp
+import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
 import numpy as np
 
-import analytic_halo.model_ionprof_js as mip
+import fire_an.analytic_halo.model_ionprof_js as mip
 import fire_an.makeplots.plot_utils as pu
+import fire_an.makeplots.tol_colors as tc
 import fire_an.utils.constants_and_units as c
 import fire_an.utils.opts_locs as ol
 
-def readin_hm_data(filen, tomatch, ion):
+def readin_hm_data(filen, tomatch, ion, mdotp_target=0.5):
     with h5py.File(mip.outdir_profiles + filen) as f:
         grpns_all = list(f.keys())
         grpns_use = [grpn for grpn in grpns_all if grpn.startswith(tomatch)]
@@ -20,24 +22,31 @@ def readin_hm_data(filen, tomatch, ion):
             if bool(grp.attrs['failed']):
                 continue
             lmvir = grp.attrs['logMvir_Msun_BN98']
+            mdotp = grp.attrs['mdot_percentile_at_Mvir']
+            if not np.isclose(mdotp, mdotp_target):
+                continue
             _sub = {}
             sgrp = grp[f'coldens_{ion}']
             _sub['b_kpc'] = sgrp['impactpar_cm'][:] / (c.cm_per_mpc * 1e-3)
             _sub['logN_cm2'] = np.log10(sgrp['coldens_cm2'][:])
+            _sub['fcgm'] = grp.attrs['fCGM']
+            _sub['mdotp'] = mdotp
+            _sub['mdot_msunpyr'] = grp.attrs['mdot_MsunperYr']
             toplot[lmvir] = _sub
     return toplot
 
 
-def plot_profiles_hm(filen, redshift, zsol, plind, mdot,
-                     outname=None):
-    tomatch = (f'z{redshift:.2f}_Zsolar{zsol:.2e}_vcplind{plind:.2f}'
-               f'mdotMsunperYr{mdot:.2e}_logmvirMsun')
-    data = readin_hm_data(filen, tomatch, 'Ne8')
-    lmvs = np.sorted(list(data.keys()))
+def plot_profiles_hm(filen, redshift, zsol, plind, outname=None):
+    tomatch = (f'z{redshift:.2f}_Zsolar{zsol:.2e}_vcplind{plind:.2f}')
+    data = readin_hm_data(filen, tomatch, 'Ne8', mdotp_target=0.5)
+    lmvs = np.sort(list(data.keys()))
+    print(lmvs)
     vmin = lmvs[0]
     vmax = lmvs[-1]
+    carray = [lmvs[0]] + [0.5 * (lmvs[i] + lmvs[i + 1]) 
+                            for i in range(len(lmvs) - 1)] + [lmvs[1]]
+    carray = np.array(carray)
     cmap = mcm.get_cmap('viridis')
-    norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
 
     fig = plt.figure(figsize=(5.5, 5.))
     grid = gsp.GridSpec(ncols=2, nrows=1, wspace=0.1, 
@@ -51,8 +60,8 @@ def plot_profiles_hm(filen, redshift, zsol, plind, mdot,
     for lmv in lmvs:
         xv = data[lmv]['b_kpc']
         yv = data[lmv]['logN_cm2']
-        plt.plot(xv, yv, color=cmap((lmv - vmin) / (vmax - vmin)),
-                 linewidth=linewidth, path_effects=path_effects)
+        ax.plot(xv, yv, color=cmap((lmv - vmin) / (vmax - vmin)),
+                linewidth=linewidth, path_effects=path_effects)
     ax.set_xlabel('$\\mathrm{r}_{\\perp} \\; [\\mathrm{pkpc}]$',
                   fontsize=fontsize)
     ax.set_ylabel('$\\log_{10} \\, \\mathrm{N}(\\mathrm{Ne\\,VIII})'
@@ -60,14 +69,257 @@ def plot_profiles_hm(filen, redshift, zsol, plind, mdot,
                   fontsize=fontsize)
     ax.tick_params(which='both', direction='in', labelsize=fontsize - 1.,
                    top=True, right=True)
-
-    plt.colorbar(mcm.ScalarMappable(norm=norm, cmap=cmap), cax=cax,
-                 orientation='vertical')
+    
+    norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
+    scm = mcm.ScalarMappable(norm=norm, cmap=cmap)
+    scm.set_array(carray)
+    plt.colorbar(scm, cax=cax, orientation='vertical')
     cax.set_ylabel('$\\log_{10} \\, \\mathrm{M}_{\\mathrm{vir}}'
                    '\\; [\\mathrm{M}_{\\odot}]$',
                    fontsize=fontsize)
     cax.tick_params(labelsize=fontsize - 1.)
     if outname is not None:
         plt.savefig(outname, bbox_inches='tight')
+
+def check_fcgm_sfr(filen, outname=None):
+    redshift = 0.75
+    zsol = 0.3
+    plind = 0.0
+    mdotp_targets = (0.16, 0.5, 0.84)
+    ls_mdotp = ('dotted', 'solid', 'dashed')
+    tomatch = (f'z{redshift:.2f}_Zsolar{zsol:.2e}_vcplind{plind:.2f}')
+    data = {}
+    for mdp_tar in mdotp_targets:
+        data[mdp_tar] = readin_hm_data(filen, tomatch, 'Ne8', 
+                                       mdotp_target=mdp_tar)
+
+    fig = plt.figure(figsize=(7.5, 3.5))
+    grid = gsp.GridSpec(ncols=2, nrows=1, wspace=0.35, 
+                        width_ratios=(1., 1.))
+    fcgmax = fig.add_subplot(grid[0])
+    sfrax = fig.add_subplot(grid[1])
+    fontsize = 12
+    linewidth = 1.5
+
+    for mdp, ls in zip(mdotp_targets, ls_mdotp):
+        label = f'{mdp*100:.0f}$^{{\\mathrm{{th}}}}$% SFR'
+        xvs = []
+        fcgms = []
+        sfrs = []
+        print(mdp)
+        print(list(data[mdp].keys()))
+        for lmv in data[mdp].keys():
+            xvs.append(lmv)
+            fcgm = data[mdp][lmv]['fcgm'] \
+                * mip.cosmo_base['omegam'] / mip.cosmo_base['omegab']
+            lsfr = np.log10(data[mdp][lmv]['mdot_msunpyr']) 
+            fcgms.append(fcgm)
+            sfrs.append(lsfr)
+        xvs = np.array(xvs)
+        po = np.argsort(xvs)
+        fcgmax.plot(xvs[po], np.array(fcgms)[po], color='black',
+                    linewidth=linewidth, linestyle=ls,
+                    label=label)
+        sfrax.plot(xvs[po], np.array(sfrs)[po], color='black',
+                   linewidth=linewidth, linestyle=ls,
+                   label=label)
+    
+    xlabel = ('$\\log_{10} \\, \\mathrm{M}_{\\mathrm{vir, BN98}}'
+              ' \\; [\\mathrm{M}_{\\odot}]$')
+    fcgmax.set_xlabel(xlabel, fontsize=fontsize)
+    sfrax.set_xlabel(xlabel, fontsize=fontsize)
+    fcgmax.set_ylabel('$\\mathrm{f}_{\\mathrm{CGM}}$',
+                      fontsize=fontsize)
+    sfrax.set_ylabel('$\\log_{10} \\, \\mathrm{SFR} \\; '
+                     '[\\mathrm{M}_{\\odot} \\mathrm{yr}^{-1}]$',
+                      fontsize=fontsize)
+    fcgmax.tick_params(which='both', direction='in', labelsize=fontsize - 1.,
+                       top=True, right=True)
+    sfrax.tick_params(which='both', direction='in', labelsize=fontsize - 1.,
+                      top=True, right=True)
+    sfrax.legend(fontsize=fontsize - 1.)
+    if outname is not None:
+        plt.savefig(outname, bbox_inches='tight')
+
+
+def check_fcgm_sfr_vars(filen, outname=None):
+    redshift = 0.75
+    zsols = [0.1, 0.3, 1.0]
+    plinds = [0.0, -0.1, -0.2]
+    mdotp_targets = (0.16, 0.5, 0.84)
+    ls_zsol = ('dotted', 'solid', 'dashed')
+    cl_plind = tc.tol_cset('vibrant')
+    lw_mdotp = [1.0, 2.0, 1.4]
+
+    fig = plt.figure(figsize=(7.5, 3.5))
+    grid = gsp.GridSpec(ncols=2, nrows=1, wspace=0.35, 
+                        width_ratios=(1., 1.))
+    fcgmax = fig.add_subplot(grid[0])
+    sfrax = fig.add_subplot(grid[1])
+    fontsize = 12
+    
+    for zsol, ls in zip(zsols, ls_zsol):
+        for plind, cl in zip(plinds, cl_plind):
+            for mdp_tar, lw in zip(mdotp_targets, lw_mdotp):
+                tomatch = (f'z{redshift:.2f}_Zsolar{zsol:.2e}'
+                           f'_vcplind{plind:.2f}_')
+                data = readin_hm_data(filen, tomatch, 'Ne8', 
+                                      mdotp_target=mdp_tar)
+
+                xvs = []
+                fcgms = []
+                sfrs = []
+                for lmv in data.keys():
+                    xvs.append(lmv)
+                    fcgm = data[lmv]['fcgm'] \
+                        * mip.cosmo_base['omegam'] / mip.cosmo_base['omegab']
+                    lsfr = np.log10(data[lmv]['mdot_msunpyr']) 
+                    fcgms.append(fcgm)
+                    sfrs.append(lsfr)
+                xvs = np.array(xvs)
+                po = np.argsort(xvs)
+                fcgmax.plot(xvs[po], np.array(fcgms)[po], color=cl,
+                            linewidth=lw, linestyle=ls)
+                sfrax.plot(xvs[po], np.array(sfrs)[po], color=cl,
+                           linewidth=lw, linestyle=ls)
+    
+    xlabel = ('$\\log_{10} \\, \\mathrm{M}_{\\mathrm{vir, BN98}}'
+              ' \\; [\\mathrm{M}_{\\odot}]$')
+    fcgmax.set_xlabel(xlabel, fontsize=fontsize)
+    sfrax.set_xlabel(xlabel, fontsize=fontsize)
+    fcgmax.set_ylabel('$\\mathrm{f}_{\\mathrm{CGM}}$',
+                      fontsize=fontsize)
+    sfrax.set_ylabel('$\\log_{10} \\, \\mathrm{SFR} \\; '
+                     '[\\mathrm{M}_{\\odot} \\mathrm{yr}^{-1}]$',
+                      fontsize=fontsize)
+    fcgmax.tick_params(which='both', direction='in', labelsize=fontsize - 1.,
+                       top=True, right=True)
+    sfrax.tick_params(which='both', direction='in', labelsize=fontsize - 1.,
+                      top=True, right=True)
+    sfrax.legend(fontsize=fontsize - 1.)
+
+    mdp_han = [mlines.Line2D((), (), color='black', linestyle='solid',
+                             linewidth=lw,
+                             label=(f'{mdp*100:.0f}$^{{\\mathrm{{th}}}}$'
+                                     '% SFR'))
+               for lw, mdp in zip(lw_mdotp, mdotp_targets)]
+    zsol_han = [mlines.Line2D((), (), color='black', linestyle=ls,
+                              linewidth=1.5,
+                              label=(f'{zsol:.1f} $\\mathrm{{Z}}'
+                                     '_{\\odot}$'))
+                for ls, zsol in zip(ls_zsol, zsols)]
+    pli_han = [mlines.Line2D((), (), color=cl, linestyle='solid',
+                             linewidth=1.5,
+                             label=('$v_{\\mathrm{c}} \\propto '
+                                    f'r^{{{plind:.1f}}}$'))
+                for cl, plind in zip(cl_plind, plinds)]
+    fcgmax.legend(handles=mdp_han + zsol_han, fontsize=fontsize - 1)
+    sfrax.legend(handles=pli_han, fontsize=fontsize - 1)
+    if outname is not None:
+        plt.savefig(outname, bbox_inches='tight')
+
+
+def plot_coldensprof_vars(filen, outname=None):
+    lmvs_tar = np.arange(11.0, 13.65, 0.2)
+
+    redshift = 0.75
+    zsols = [0.1, 0.3, 1.0]
+    plinds = [0.0, -0.1, -0.2]
+    mdotp_targets = (0.16, 0.5, 0.84)
+    ls_zsol = ('dotted', 'solid', 'dashed')
+    cl_plind = tc.tol_cset('vibrant')
+    lw_mdotp = [1.0, 2.0, 1.4]
+    
+    npanels = len(lmvs_tar)
+    panelsize = 2.5
+    ncols_max = 4
+    ncols = min(ncols_max, npanels)
+    nrows = (npanels - 1) // ncols + 1
+    hspace = 0.2
+    wspace = 0.2
+    width_ratios = [panelsize] * ncols
+    height_ratios = [panelsize] * nrows
+    width = sum(width_ratios) * (1. + wspace * (ncols - 1.) / ncols)
+    height = sum(height_ratios) * (1. + hspace * (nrows - 1.) / nrows)
+
+    fig = plt.figure(figsize=(width, height))
+    grid = gsp.GridSpec(ncols=ncols, nrows=nrows, wspace=wspace, 
+                        hspace=hspace,
+                        width_ratios=width_ratios,
+                        height_ratios=height_ratios)
+    axes = [fig.add_subplot(grid[i // ncols, i % ncols]) 
+            for i in range(npanels)]
+    fontsize = 12
+    
+    for zsol, ls in zip(zsols, ls_zsol):
+        for plind, cl in zip(plinds, cl_plind):
+            for mdp_tar, lw in zip(mdotp_targets, lw_mdotp):
+                tomatch = (f'z{redshift:.2f}_Zsolar{zsol:.2e}'
+                           f'_vcplind{plind:.2f}_')
+                data = readin_hm_data(filen, tomatch, 'Ne8', 
+                                      mdotp_target=mdp_tar)
+                lmvs = [key for key in data 
+                        if np.any(np.isclose(key, lmvs_tar))]
+                for lmv in lmvs:
+                    axi = np.where(np.isclose(lmv, lmvs_tar))[0][0]
+                    xv = data[lmv]['b_kpc']
+                    yv = data[lmv]['logN_cm2']
+                    axes[axi].plot(xv, yv, color=cl, linewidth=lw, 
+                                   linestyle=ls)
+    for axi in range(npanels):
+        ax = axes[axi]
+        doleft = axi % ncols == 0
+        dobottom = axi >= npanels - ncols
+        ax.tick_params(which='both', direction='in', right=True,
+                       top=True)
+        if doleft:
+            ax.set_ylabel('$\\log_{10} \\, \\mathrm{N}(\\mathrm{Ne\\,VIII})'
+                          '\\; [\\mathrm{cm}^{-2}]$', fontsize=fontsize)
+        if dobottom:
+            ax.set_xlabel('$\\mathrm{r}_{\\perp} \\; [\\mathrm{pkpc}]$',
+                          fontsize=fontsize)
+        axtitle = ('$\\mathrm{M}_{\\mathrm{vir}} ='
+                   f'10^{{{lmvs_tar[axi]:.1f}}}'
+                   '\\, \\mathrm{M}_{\\odot}$')
+        ax.text(0.95, 0.95, axtitle, fontsize=fontsize, 
+                transform=ax.transAxes, horizontalalignment='right',
+                verticalalignment='top')
+    
+    mdp_han = [mlines.Line2D((), (), color='black', linestyle='solid',
+                             linewidth=lw,
+                             label=(f'{mdp*100:.0f}$^{{\\mathrm{{th}}}}$'
+                                     '% SFR'))
+               for lw, mdp in zip(lw_mdotp, mdotp_targets)]
+    zsol_han = [mlines.Line2D((), (), color='black', linestyle=ls,
+                              linewidth=1.5,
+                              label=(f'{zsol:.1f} $\\mathrm{{Z}}'
+                                     '_{\\odot}$'))
+                for ls, zsol in zip(ls_zsol, zsols)]
+    pli_han = [mlines.Line2D((), (), color=cl, linestyle='solid',
+                             linewidth=1.5,
+                             label=('$v_{\\mathrm{c}} \\propto '
+                                    f'r^{{{plind:.1f}}}$'))
+                for cl, plind in zip(cl_plind, plinds)]
+    if npanels >= 3:
+        axes[0].legend(handles=mdp_han, fontsize=fontsize - 1)
+        axes[1].legend(handles=zsol_han, fontsize=fontsize - 1)
+        axes[2].legend(handles=pli_han, fontsize=fontsize - 1)
+    elif npanels == 2:
+        axes[0].legend(handles=mdp_han + zsol_han, 
+                       fontsize=fontsize - 1)
+        axes[1].legend(handles=pli_han, fontsize=fontsize - 1)
+    elif npanels == 1:
+        axes[0].legend(handles=mdp_han + zsol_han + pli_han, 
+                       fontsize=fontsize - 1)
+    
+    if outname is not None:
+        plt.savefig(outname, bbox_inches='tight')
+
+        
+
+                
+
+
+
 
     

80f181380853aed9ff959d4998b4b69ae0925be0
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2024-02-06 19:05:48 -0600

    add run test4

--- spectra/queuespectra.py
+++ spectra/queuespectra.py
@@ -1,8 +1,15 @@
 import sys
 
-import genspectra as gs
+import fire_an.simlists as sl
+import fire_an.spectra.genspectra as gs
+
+## Notes on requirements:
+# on Quest, 200 sightlines ran in a bit under 6 hours (1 node/1 CPU)
 
 def runtest2():
+    '''
+    first look: spectra around 1 halo
+    '''
     simname = 'crheatfix_m12f_r7100'
     snapnum = 277
     obase = f'/test2/tridentray_{simname}_{snapnum}'
@@ -13,9 +20,35 @@ def runtest2():
     gs.runsightlines(simname, snapnum, outname_base=obase,
                       settype='grid', **setargs)
     
+def runtest4(ind):
+    '''
+    second look: multiple haloes,
+    but only FIRE-2 core, 2 redshifts, 1 axis,
+    still using the FG09 UV/X-ray bkg, 
+    working on the yt dl fix branch
+    '''
+
+    simnames = sl.m12_f2md
+    snapshots = [sl.snaps_f2md[0], sl.snaps_f2md[1]]
+
+    snapi = ind % len(snapshots)
+    simi = ind // len(snapshots)
+
+    simname = simnames[simi]
+    snapnum = snapshots[snapi]
+    outbase = f'/test4/tridentray_{simname}_{snapnum}'
+    setargs = {'totlength': 4.,
+               'gridside': 4.,
+               'gridpoints_side': 20, 
+               'axis':'z'}
+    gs.runsightlines(simname, snapnum, outname_base=outbase,
+                      settype='grid', **setargs)
+    
 def main(ind):
     if ind == 0:
         runtest2()
+    elif ind >= 1 and ind < 21:
+        runtest4(ind - 1)
     else:
         raise ValueError(f'nothing specified for index {ind}')
     

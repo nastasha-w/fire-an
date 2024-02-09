53dd4e0705a180534d8636262478523d2f72cc4b
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2024-02-08 22:48:15 -0600

    debug from laptop

--- spectra/explorespectra.py
+++ spectra/explorespectra.py
@@ -49,7 +49,7 @@ def plotoverview_spectra(filepattern: str,
             pgal_cm = f['Header/cengal'].attrs['pcen_cm']
             vcom_cmps = f['Header/cengal'].attrs['vcom_cmps']
             hpar = cu.Hubble(cosmopars)
-            axis = np.string(f['Header/sample'].attrs['axis'])
+            axis = str(f['Header/sample'].attrs['axis'])
             xaxi, yaxi, losaxi = gs.getinds_ax(axis)
             starts = f['startpos_cm'][:]
             ends = f['endpos_cm'][:]

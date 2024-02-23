b2309a6d39585d827b1f9cd792f2a9e9e36353f1
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2024-02-23 17:28:50 -0600

    added CUBS data comp for sigma_Ne8

--- spectra/plots/plot_coldens_sigmav.py
+++ spectra/plots/plot_coldens_sigmav.py
@@ -6,12 +6,11 @@ import pandas as pd
 
 import fire_an.makeplots.plot_utils as pu
 import fire_an.makeplots.tol_colors as tc
+import fire_an.spectra.plots.select_obsdata as so
 
-# TODO: actually do the data comparison
-# TODO: check whether tau-wtd v or galaxy v is the obs. zero point
+# TODO: actually do the data comparison for CASBaH
+# Zhijie: CUBS sigmas are rel. to tau-weighted velocity
 def datacomp(simfilen: str,
-             cubsfilen: str | None = None,
-             casbahfilen: str | None = None,
              outfilen: str | None = None):
     fmtsim = {'color': 'gray',
               'marker': 'o',
@@ -19,12 +18,12 @@ def datacomp(simfilen: str,
               'alpha': 0.5}
     cs = tc.tol_cset('bright')
     fmtcubs = {'color': cs[0],
-              'marker': 's',
-              's': 10.,
-              'alpha': 1.}
+               'marker': 's',
+               'markersize': 5.,
+               'alpha': 1.}
     fmtcasbah = {'color': cs[1],
                  'marker': 'p',
-                 's': 10.,
+                 'markersize': 5.,
                  'alpha': 1.}
     simdata = pd.read_csv(simfilen, sep='\t')
     fig = plt.figure(figsize=(5., 5.))
@@ -33,13 +32,13 @@ def datacomp(simfilen: str,
     
     ax.set_xlabel('$\\log_{10} \\, \\mathrm{N} \\; [\\mathrm{cm}^{-2}]$',
                   fontsize=fontsize)
-    ax.set_ylabel('$\\sigma(v - v_{\\mathrm{gal}}) \\; '
+    ax.set_ylabel('$\\sigma\\left(v - \\langle v \\rangle_{\\tau}\\right) \\; '
                   '[\\mathrm{km}/\\mathrm{s}]$',
                   fontsize=fontsize)
     ax.tick_params(direction='in', which='both', labelsize=fontsize - 1.,
                    left=True, top=True)
     xp = simdata['Ntot_logcm2']
-    yp = simdata['sigmav_galv_kmps']
+    yp = simdata['sigmav_kmps']
     ax.scatter(xp, yp,
                label='FIRE-2', rasterized=True, **fmtsim)
     binsize = 0.1
@@ -52,7 +51,6 @@ def datacomp(simfilen: str,
                            mincount_x=20,
                            getoutliers_y=False, getmincounts_x=False,
                            x_extremes_only=False)
-    print(xcen.shape, yperc.shape)
     ax.plot(xcen[xsel], (yperc[1:-1, 0])[xsel],
             color='black', linestyle='dashed',
             linewidth=1.2, label='FIRE-2: 10, 90%')
@@ -62,7 +60,41 @@ def datacomp(simfilen: str,
     ax.plot(xcen[xsel], (yperc[1:-1, 1])[xsel], 
             color='black', linestyle='solid',
             linewidth=1.5, label='FIRE-2: median')
-    ax.legend(fontsize=fontsize)
+    
+    cubssys = so.selectsystems(simdata['Mvir_logMsun'],
+                               np.array([0.5, 1.0]),
+                               'cubs',
+                               Mhmargin_dex=0.2,
+                               zmargin=0.05,
+                               method='bestest_inrange')
+    isul_sigma = np.isnan(cubssys['sigmav [km/s]'])
+    noul_sigma = np.logical_not(isul_sigma)
+    cubssys_uls = cubssys[isul_sigma]
+    cubssys_dets = cubssys[noul_sigma]
+    ax.errorbar(cubssys_dets['Nline [logcm2]'],
+                cubssys_dets['sigmav [km/s]'],
+                xerr=(cubssys_dets['err_Nline_minus [dex]'],
+                      cubssys_dets['err_Nline_plus [dex]']),
+                yerr=(cubssys_dets['err_sigmav_minus [km/s]'],
+                      cubssys_dets['err_sigmav_plus [km/s]']),
+                linestyle='none',
+                label='CUBS',
+                **fmtcubs)
+    ax.errorbar(cubssys_uls['Nline [logcm2]'],
+                cubssys_uls['err_sigmav_plus [km/s]'],
+                xerr=(cubssys_uls['err_Nline_minus [dex]'],
+                      cubssys_uls['err_Nline_plus [dex]']),
+                linestyle='none',
+                **fmtcubs)
+    for i in cubssys_uls.index:
+        # arrow head width/length seem to be in data units
+        ax.arrow(cubssys_uls.at[i, 'Nline [logcm2]'],
+                cubssys_uls.at[i, 'err_sigmav_plus [km/s]'],
+                0, -15., head_width=0.05, head_length=3.,
+                color=fmtcubs['color'], alpha=fmtcubs['alpha'])
+    ax.set_xlim(12.4, 14.8)
+    ax.set_ylim(0., 250.)
+    ax.legend(fontsize=fontsize - 1., handlelength=1.5)
 
     if outfilen is not None:
         plt.savefig(outfilen, bbox_inches='tight')
@@ -85,6 +117,10 @@ def plotpanel_colby(ax, cax, df, cqty, fontsize=12):
         ccol = 'sigmav_kmps'
         clabel = ('$\\sigma(v - \\langle v \\rangle_{\\tau})'
                   '\\; [\\mathrm{km} / \\mathrm{s}]$')
+    elif cqty == 'sigmav_gal':
+        ccol = 'sigmav_galv_kmps'
+        clabel = ('$\\sigma(v - v_{\\mathrm{gal}})'
+                  '\\; [\\mathrm{km} / \\mathrm{s}]$')
     cmapn = 'rainbow'
     cdat = df[ccol]
     vmin = np.min(cdat)
@@ -96,7 +132,7 @@ def plotpanel_colby(ax, cax, df, cqty, fontsize=12):
                     extend='neither', orientation='vertical')
 
     xp = df['Ntot_logcm2']
-    yp = df['sigmav_galv_kmps']
+    yp = df['sigmav_kmps']
     ax.scatter(xp, yp, c=cp, marker='o', s=3., alpha=0.5,
                label='FIRE-2', rasterized=True, )
     binsize = 0.1
@@ -139,7 +175,8 @@ def plot3rdfactor_N_sigma(simdatafilen: str,
     plotpanel_colby(axes[0], axes[1], simdata, 'Mvir', fontsize=fontsize)
     plotpanel_colby(axes[2], axes[3], simdata, 'Mstar', fontsize=fontsize)
     plotpanel_colby(axes[4], axes[5], simdata, 'ipar', fontsize=fontsize)
-    plotpanel_colby(axes[6], axes[7], simdata, 'sigmav', fontsize=fontsize)
+    plotpanel_colby(axes[6], axes[7], simdata, 'sigmavgal', 
+                    fontsize=fontsize)
     axes[0].legend(fontsize=fontsize - 1.)
 
     for axi, ax in enumerate(axes):
@@ -154,7 +191,7 @@ def plot3rdfactor_N_sigma(simdatafilen: str,
                            ' [\\mathrm{cm}^{-2}]$',
                            fontsize=fontsize)
         if doleft:
-            ax.set_ylabel('$\\sigma(v - v_{\\mathrm{gal}}) \\; '
+            ax.set_ylabel('$\\sigma(v - \\langle v \\rangle_{\\tau}) \\;'
                           '[\\mathrm{km}/\\mathrm{s}]$',
                           fontsize=fontsize)
         ax.tick_params(direction='in', which='both',
--- spectra/plots/select_obsdata.py
+++ spectra/plots/select_obsdata.py
@@ -1,9 +1,14 @@
 # stub for obs. selection based on halo mass and redshift
 import numpy as np
+import pandas as pd
 import scipy.special as sps
 
 import fire_an.mstar_mhalo.loader_smdpl_sfr as ldsmdpl
 import fire_an.utils.math_utils as mu
+
+obsdatadir = '/projects/b1026/nastasha/extdata/highuv_velocities/'
+filen_cubs_components = obsdatadir + 'ne8_detected_components_cubs.dat'
+filen_cubs_systems = obsdatadir + 'ne8_detected_systems_cubs.dat'
 
 def getranges_Mh_z(logMh_sim: np.ndarray[float],
                    z_sim: np.ndarray[float],
@@ -73,10 +78,96 @@ def getMhest(logMstar_meas: float,
             for cp in cumulP_hi]
     return bestest, mlo, mhi
 
-def getMhest_cubs(cubsdatadict):
-    mss = cubsdatadict['mstar_log10Msun']
-    mss_hierr_1s = 0.5 * cubsdatadict['mstar_2s_hierr_dex']
-    mss_loerr_1s = 0.5 * cubsdatadict['mstar_2s_loerr_dex']
+def getMhest_cubs(cubssys: pd.DataFrame,
+                  err_sigma: tuple[float] = (1, 2)):
+    '''
+    Returns: 
+    --------
+    pandas Dataframe
+        input dataframe with errors at requested sigma levels added.
+        columns are 
+        'Mvir_bestest [log10 Msun]'
+        'err_Mvir_<err_sigma>_plus [dex]'
+        'err_Mvir_<err_sigma>_minus [dex]'
+        for all requested err_sigma levels
+    '''
+    # assuming 1-sigma error bars in preprint Table B.1
+    mss = cubssys['Mstar [log10Msun]']
+    mss_hierr_1s = cubssys['err_Mstar_plus [dex]']
+    mss_loerr_1s = cubssys['err_Mstar_minus [dex]']
+    zsgal = cubssys['zgal']
+    #mss_hierr_1s = 0.5 * cubsdatadict['mstar_2s_hierr_dex']
+    #mss_loerr_1s = 0.5 * cubsdatadict['mstar_2s_loerr_dex']
     calscatter = 0.2 # scatter in SED-fit to few-band M* calibration
     mss_hierr_1s = np.sqrt(mss_hierr_1s**2 + calscatter**2)
     mss_loerr_1s = np.sqrt(mss_loerr_1s**2 + calscatter**2)
+    
+    mhs = []
+    mh_loerrs = []
+    mh_hierrs = []
+    for ms, loerr_1s, hierr_1s, zgal in zip(
+            mss, mss_hierr_1s, mss_loerr_1s, zsgal):
+        mh, mh_hierr, mh_loerr = getMhest(
+            ms, (loerr_1s, hierr_1s), zgal, sigmas_tar=err_sigma)
+        mhs.append(mh)
+        mh_loerrs.append(mh_loerr)
+        mh_hierrs.append(mh_hierr)
+    mhs = np.array(mhs)
+    mh_hierrs = np.array(mh_hierrs).T
+    mh_loerrs = np.array(mh_loerrs).T
+    cubssys['Mvir_bestest [log10 Msun]'] = mhs
+    for si, s in enumerate(err_sigma):
+        cubssys[f'err_Mvir_{s:.1f}_plus'] = mh_hierrs[si] 
+        cubssys[f'err_Mvir_{s:.1f}_minus'] = mh_loerrs[si]
+    return cubssys
+
+def selectsystems(logMh_sim: np.ndarray[float],
+                  z_sim: np.ndarray[float],
+                  dataset: {'cubs', 'casbah'},
+                  Mhmargin_dex: float = 0.2,
+                  zmargin: float = 0.05,
+                  method: str = 'bestest_inrange'):
+    '''
+    Parameters:
+    -----------
+    method: str
+        'inrange_bestest': zgal and mode of the Mvir distribution
+                           lie within the margins for both
+        'inrange_sigma_<n>': zgal lies in z range (with margin),
+                             Mvir n-sigma range overlaps the 
+                             simulation Mvir range (with margin)
+    '''
+    minlogMh, maxlogMh, minz, maxz =  getranges_Mh_z(
+            logMh_sim, z_sim, Mhmargin_dex=Mhmargin_dex, zmargin=zmargin)
+    if method == 'bestest_inrange':
+        nsig = 1. # just use some default
+    elif method.startswith('inrange_sigma_'):
+        nsig = float(method.split('_')[-1])
+
+    if dataset == 'cubs':
+        df = pd.read_csv(filen_cubs_systems, sep='\t', comment='#',
+                         converters={'sigmav [km/s]': np.float64,
+                                     'QSO': str,
+                                     'system': str,
+                                     'impact_parameter [kpc]': np.float64,
+                                     })
+        getMhest_cubs(df, err_sigma=(nsig,))
+    elif dataset == 'casbah':
+        raise NotImplementedError('Casbah read-in: TODO')
+    
+    if method == 'bestest_inrange':
+        sel = np.ones(len(df['Mvir_bestest [log10 Msun]']), dtype=bool)
+        sel &= df['zgal'] <= maxz
+        sel &= df['zgal'] >= minz
+        sel &= df['Mvir_bestest [log10 Msun]'] <= maxlogMh
+        sel &= df['Mvir_bestest [log10 Msun]'] >= minlogMh
+    elif method.startswith('inrange_sigma_'):
+        sel = np.ones(len(df['Mvir_bestest [log10 Msun]']), dtype=bool)
+        sel &= df['zgal'] <= maxz
+        sel &= df['zgal'] >= minz
+        sel &= df[f'err_Mvir_{nsig:.1f}_minus'] <= maxlogMh
+        sel &= df[f'err_Mvir_{nsig:.1f}_plus'] >= minlogMh
+    return df[sel]
+
+
+        

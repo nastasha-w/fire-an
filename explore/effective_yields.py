301d7b078eb1567bd1e32eca569c2e7535bbada1
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2024-04-05 15:34:00 -0500

    worked on plots

--- explore/effective_yields.py
+++ explore/effective_yields.py
@@ -6,6 +6,7 @@ zoom simulation volumes
 import h5py
 import matplotlib.gridspec as gsp
 import matplotlib.pyplot as plt
+from matplotlib.transforms import Transform
 import numpy as np
 
 import fire_an.mainfunc.get_qty as gq
@@ -99,10 +100,135 @@ def run_totals(index):
 
     get_totals(simname, snapnum, outname)
             
-def get_yielddata(simname):
-    pass
+def get_yielddata(simname, species='total', source='all'):
+    snapnums = snaps_hr if simname in sims_hr \
+               else snaps_sr if simname in sims_sr \
+               else snaps_md if simname in sims_md \
+               else None
+    zs = []
+    yields = []
+    if source == 'all':
+        sources = ['gas', 'stars']
+    else:
+        sources = [source]
+    for i, snapnum in enumerate(snapnums):
+        filen = ('/projects/b1026/nastasha/hists/gas_stars_metals/'
+                 f'total_MassZ_stars_gas_{simname}_{snapnum}.hdf5')
+        zkey = snaps_z[i]
+        zs.append(zkey)
+        with h5py.File(filen, 'r') as f:
+            mstar = f['stars/Mass'].attrs('total mass')
+            conv = f['stars/Mass'].attrs('toCGS')
+            mstar *= conv
+            mZ = 0.
+            for source in sources:
+                path = f'{source}/MetalMass_{species}'
+                _mZ = f[path].attrs('total mass')
+                _conv = f[path].attrs('toCGS')
+                if species != 'total':
+                    _conv *= gq.elt_atomw_cgs(species)
+                mZ += _mZ * _conv
+        yields.append(mZ / mstar)
+    return zs, yields
+    
 
 def plotyields(species='total', source='all'):
-    pass
+    simnames_all = sl.m12_hr_all2 + sl.m13_hr_all2 \
+                   + sl.m12_sr_all2 + sl.m13_sr_all2 \
+                   + sl.m12_fire3x_tests + sl.m12_f2md
+    bugsims = sl.buglist2
+    ics = [sl.ic_from_simname(sn) for sn in simnames_all]
+    ics_all = np.unique(ics)
+    ics_all.sort()
+    ics_special = ['m12f', 'm12m', 'm12i']
+    allyields_bug = {}
+    specialyields_bug = {}
+    allyields_nobug = {}
+    specialyields_nobug = {}
+    
+    fontsize = 12
+    npanels = len(ics_all)
+    ncols = min(npanels, 4)
+    nrows = (npanels - 1) // ncols + 1
+    _nrows = nrows + 1
+    panelsize = 2.5
+    hspace = 0.4
+    wspace = 0.
+    width = panelsize * ncols * (1. + (ncols - 1.) * wspace / ncols)
+    height = panelsize * _nrows * (1. + hspace / _nrows)
+
+    fig = plt.figure(figsize=(width, height))
+    _grid = gsp.GridSpec(nrows=2, ncols=1, 
+                         height_ratios=[nrows, 1], hspace=hspace)
+    grid = gsp.GridSpecFromSubplotSpec(nrows=nrows, ncols=ncols, 
+                                       hspace=0., wspace=0.,
+                                       subplot_spec=_grid[0])
+    mainaxes = [fig.add_subplot(grid[i // ncols, i % ncols]) 
+                for i in range(npanels)]
+    hgrid = gsp.GridSpecFromSubplotSpec(nrows=1, ncols=3, 
+                                        hspace=0., wspace=0.,
+                                        subplot_spec=_grid[1])
+    haxes = [fig.add_subplot(hgrid[0, i]) 
+             for i in range(3)]
+    
+    title1 = 'total metal mass' if species =='total' \
+             else f'{species} mass'
+    title2 = ' in stars and gas' if source == 'all' \
+             else f' in {source}'
+    title = title1 + title2 + ' / current stellar mass'
+    fig.suptitle(title)
+    
+    for axi, ic in enumerate(ics_all):
+        dobottom = axi >= npanels - ncols
+        doleft = axi % ncols == 0
+        ax = mainaxes[axi]
+        ax.tick_params(which='both', labelsize=fontsize - 1,
+                       direction='in', right=True, top=True,
+                       grid=True, labelbottom=dobottom,
+                       labelleft=doleft)
+        if dobottom:
+            ax.set_xlabel('redshift', fontsize=fontsize)
+        if doleft:
+            ax.set_ylabel('$\\mathrm{M}_{\\mathrm{Z}} \\,/\\,'
+                          '\\mathrm{M}_{\\star}$')
+        ax.text(0.95, 0.95, ic, fontsize=fontsize - 1.,
+                transform=ax.transAxes,
+                horizontalalignment='right',
+                verticalalignment='top')
+        for simname in simnames_all:
+            if sl.ic_from_simname(simname) != ic:
+                continue
+            phys = sl.physlabel_from_simname(simname)
+            hasbug = simname in bugsims
+            color = physcolors[phys]
+            linestyle = 'dashed' if hasbug else 'solid'
+            zs, yields = get_yielddata(simname, species=species,
+                                       source=source)
+            ax.plot(zs, yields, color=color, linestyle=linestyle,
+                    linewidth=1.5, marker='o')
+            if hasbug:
+                if ic in ics_special:
+                    if phys in specialyields_bug:
+                        specialyields_bug[phys] += yields
+                    else:
+                        specialyields_bug[phys] = yields
+                if phys in allyields_bug:
+                    allyields_bug[phys] += yields
+                else:
+                    allyields_bug[phys] = yields
+            else:
+                if ic in ics_special:
+                    if phys in specialyields_nobug:
+                        specialyields_nobug[phys] += yields
+                    else:
+                        specialyields_nobug[phys] = yields
+                if phys in allyields_nobug:
+                    allyields_nobug[phys] += yields
+                else:
+                    allyields_nobug[phys] = yields
+                
 
 
+
+
+    

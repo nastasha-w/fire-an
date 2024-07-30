060d0b119df5d9a69255b01a7e34f076c6e1667c
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2024-07-29 19:23:18 -0500

    (partial?) debug rough_detmasks

--- spectra/procspectra/rough_detmasks.py
+++ spectra/procspectra/rough_detmasks.py
@@ -9,7 +9,7 @@ def get_roughdetmask(spectrum: fc.SpectrumFitBayes,
                      lsf_sigma_kmps: float = 30.,
                      coldetlim_logN_cm2: float = 14.,
                      maxbpar_kmps: float = 100.,
-                     mincutfrac: float = 1e-2):
+                     mincutfrac: float = 0.05):
     '''
     untested draft
     '''
@@ -54,54 +54,71 @@ def get_roughdetmask(spectrum: fc.SpectrumFitBayes,
     cutlevel = 1. - (1. - minlevel) * mincutfrac
     cutinds = []
     vcross = mu.find_intercepts(convflux, vvals, cutlevel)
-    # crosses cutnlevel between index and index + 1
+    # crosses cutlevel between index and index + 1
     crossinds = np.searchsorted(vvals, vcross) - 1
+    print('enclinds: ', enclinds)
+    print('crossinds: ', crossinds)
     for ai, encli in enumerate(enclinds):
+        print(ai)
         cuti = [None, None]
         # first component: region starts at zero or first crossing
-        if ai == 0: 
-            if crossinds[0] < encli[0]:
-                cuti[0] = crossinds[0]
+        if ai == 0:
+            if len(crossinds) == 0:
+                cuti[0] = 0
+            elif crossinds[0] < encli[0]:
+                cuti[0] = np.max(crossinds[crossinds < encli[0]])
             else:
                 cuti[0] = 0
         # last component: region ends at array end or last crossing
         if ai == len(enclinds) - 1: 
-            if crossinds[-1] > encli[1] - 1:
-                cuti[1] = crossinds[-1] + 1
+            if len(crossinds) == 0:
+                cuti[1] = len(vvals)
+            elif crossinds[-1] > encli[1] - 1:
+                cuti[1] = np.min(crossinds[crossinds > encli[1]]) + 1
             else:
                 cuti[1] = len(vvals)
         # if a start (end) is not set, there is a component before
         # (after) this one
-        if cuti[0] is not None:
+        if cuti[0] is None:
             lastend = enclinds[ai - 1][1]
-            closestcross = np.max(crossinds[crossinds < encli[ai][0]])
-            # cutlevel crossing closest to component start
-            if closestcross > lastend: 
+            cisel = np.logical_and(crossinds <= encli[0],
+                                   crossinds > lastend)
+            crossopts = crossinds[cisel]
+            if len(crossopts) > 0:
+                closestcross = np.max(crossopts)
+                 # cutlevel crossing closest to component start
                 cuti[0] = closestcross
-            else: # minimum between components
-                cuti[0] = np.argmax(convflux[lastend : enclinds[ai][0]])
-                cuti[0] = cuti[0] + lastend
-        if cuti[1] is not None:
+            elif lastend == encli[0]: 
+                cuti[0] = encli[0]
+            else: # maximum between components
+                cuti[0] = np.argmax(convflux[lastend : encli[0]])
+                cuti[0] = cuti[0] + lastend + 1
+        if cuti[1] is None:
             nextstart = enclinds[ai + 1][0]
-            closestcross = np.mix(crossinds[crossinds >= encli[ai][1]])
-            if closestcross < nextstart: 
+            cisel = np.logical_and(crossinds + 1 < nextstart,
+                                   crossinds >= encli[1])
+            crossopts = crossinds[cisel]
+            if len(crossopts) > 0: 
+                closestcross = np.min(crossopts)
                 cuti[1] = closestcross + 1
+            elif encli[1] == nextstart:
+                cuti[1] = nextstart
             else:
-                cuti[1] = np.argmax(convflux[enclinds[ai][1] : nextstart])
-                cuti[1] = cuti[1] + enclinds[ai][1] 
+                cuti[1] = np.argmax(convflux[encli[1] : nextstart])
+                cuti[1] = cuti[1] + encli[1] + 1
         cutinds.append(cuti)
     # check each 'component' for detectability (raw spectrum)
-    enclinds_det = []
+    cutinds_det = []
     cds = []
     mask = np.zeros(len(vvals), dtype=bool)
     for imin, imax in cutinds:
         sel = slice(imin, imax, None)
         cd = line.tau_to_coldens(spectrum.tau_raw[sel], vvals[sel])
         if cd >= 10**coldetlim_logN_cm2:
-            enclinds_det.append([imin, imax])
+            cutinds_det.append([imin, imax])
             cds.append(cd)
             mask[sel] = True
-    return mask, enclinds_det, cds
+    return mask, cutinds_det, cds
     
 def test_rough_detmasks(spectrum: fc.SpectrumFitBayes,
                         outname: str | None = None,
@@ -126,7 +143,7 @@ def test_rough_detmasks(spectrum: fc.SpectrumFitBayes,
         label = 'detected' if ci == 0 else None
         ax.plot(vvals[sl], rawspec[sl], linestyle='dashed', color='red',
                 linewidth=1.5, label=label)
-        vcen = 0.5 * (vvals[imin] + vvals[imax])
+        vcen = 0.5 * (vvals[imin] + vvals[imax - 1])
         ax.text(vcen, 1.02, '$ \\log_{10} \\, \\mathrm{N} = '
                 f'{np.log10(cds[ci]):.2f}$',
                 horizontalalignment='center',
@@ -141,6 +158,7 @@ def test_rough_detmasks(spectrum: fc.SpectrumFitBayes,
     ax.set_ylabel('transmission', fontsize=12)
     ax.tick_params(which='both', direction='in', labelsize=11.,
                    top=True, right=True)
+    ax.legend()
     if outname is not None:
         plt.savefig(outname, bbox_inches='tight')
 
@@ -149,20 +167,20 @@ def runtest_rough_detmasks():
     # some pretty much random sightlines,
     # but close to the galaxy for hopefully higher column densities
     outdir = '/projects/b1026/nastasha/tests/absspec_rough_compdet/'
-    outname_temp = 'test_compdet_ne8_770_{specname}_lsf30_detlim13p0_maxb50.pdf'
+    outname_temp = 'test_compdet_ne8_770_{specname}_lsf30_detlim13p5_maxb50.pdf'
     specdir = '/projects/b1026/nastasha/spectra/test4/'
     specfiles = ['tridentray_m12z_r4200_294_210.txt',
-                 #'tridentray_m12z_r4200_294_208.txt',
-                 #'tridentray_m12z_r4200_294_212.txt',
-                 #'tridentray_m12w_r7100_294_210.txt',
-                 #'tridentray_m12w_r7100_294_208.txt',
-                 #'tridentray_m12w_r7100_294_212.txt',
-                 #'tridentray_crheatfix_m12f_r7100_294_210.txt',
-                 #'tridentray_crheatfix_m12f_r7100_294_208.txt',
-                 #'tridentray_crheatfix_m12f_r7100_294_212.txt',
-                 #'tridentray_m12m_r7100_294_210.txt',
-                 #'tridentray_m12m_r7100_294_208.txt',
-                 #'tridentray_m12m_r7100_294_212.txt',
+                 'tridentray_m12z_r4200_294_208.txt',
+                 'tridentray_m12z_r4200_294_212.txt',
+                 'tridentray_m12w_r7100_294_210.txt',
+                 'tridentray_m12w_r7100_294_208.txt',
+                 'tridentray_m12w_r7100_294_212.txt',
+                 'tridentray_crheatfix_m12f_r7100_294_210.txt',
+                 'tridentray_crheatfix_m12f_r7100_294_208.txt',
+                 'tridentray_crheatfix_m12f_r7100_294_212.txt',
+                 'tridentray_m12m_r7100_294_210.txt',
+                 'tridentray_m12m_r7100_294_208.txt',
+                 'tridentray_m12m_r7100_294_212.txt',
                  ]
                  
 
@@ -173,5 +191,5 @@ def runtest_rough_detmasks():
         test_rough_detmasks(spectrum,
                             outname=outname,
                             lsf_sigma_kmps=30.,
-                            coldetlim_logN_cm2=13.0,
+                            coldetlim_logN_cm2=13.5,
                             maxbpar_kmps=50.)
